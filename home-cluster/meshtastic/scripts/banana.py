ecb250d2c2d8e629ca8e99dc0be7fbec8746c762
Steve <13557887+kg6zjl@users.noreply.github.com>
2026-09-17 23:33:14 -0700

    Update banana.py

--- home-cluster/meshtastic/scripts/banana.py
+++ home-cluster/meshtastic/scripts/banana.py
@@ -20,9 +20,14 @@ if len(sys.argv) > 2:
 else:
     trigger = os.environ.get("MESSAGE", "banana")
 
+# Extract the first word, cleaned
 trigger = trigger.strip().lower().split()[0]
-emoji = taco_bot_map.get(trigger, "🍌")
 
+# Safely exit if the word is not a valid option
+if trigger not in taco_bot_map:
+    sys.exit(0)
+
+emoji = taco_bot_map[trigger]
 response = "❌" + emoji if hops == 0 else emoji * hops
 
 print(response)
